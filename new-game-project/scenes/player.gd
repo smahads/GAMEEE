d485ccc3ad10cad4bc4499809af254c198a239be
smahads <smahads13@gmail.com>
2025-09-27 15:24:49 +0500

    fixed error

--- new-game-project/scenes/player.gd
+++ new-game-project/scenes/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -19,9 +20,12 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("move_left", "move_right")
 	#flip the sprite
 	if direction > 0:
-		AnimatedSprite2D.flip_h = false
+		animated_sprite.flip_h = false
 	elif direction < 0:
-		AnimatedSprite2D.flip_h = true
+		animated_sprite.flip_h = true
+		
+	#play animations
+	
 	
 	if direction:
 		velocity.x = direction * SPEED
