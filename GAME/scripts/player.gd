c4fa996925921dfc952549f896b1e9c6e1578b59
t4taimur <t4taimur2007@gmail.com>
2025-09-28 13:19:18 +0500

    laivel3 done

--- GAME/scripts/player.gd
+++ GAME/scripts/player.gd
@@ -2,7 +2,7 @@ class_name Player extends CharacterBody2D
 
 
 const SPEED = 150.0
-const JUMP_VELOCITY = -350.0
+const JUMP_VELOCITY = -420.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
